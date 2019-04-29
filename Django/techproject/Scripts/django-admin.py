e3c8e3d7f8b4c9a69fe743f88f09b05db970edd9
TimmyC101 <tcalhoun101@yahoo.com>
2019-04-29 11:42:39 -0600

    Django part 1

---
+++ Django/techproject/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\tim\documents\github\python\django\techproject\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
